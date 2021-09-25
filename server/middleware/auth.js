9963b4ed9030d61f7301105e57ed428407ae29d0
geforce6t <shashwat.live@outlook.com>
2021-09-26 00:00:37 +0530

    feat(auth): add auth routes and middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).json({ message: "A token is required for authentication"});
+  }
+  try {
+    const decoded = jwt.verify(token, prcoess.env.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).json({message: "Invalid Token"});
+  }
+  return next();
+};
--- server/models/user.js
+++ server/models/user.js
@@ -4,10 +4,11 @@ const userSchema = new mongoose.Schema({
     name: String,
     password: String,
     mail: String,
-    Type: {
+    type: {
         type: String,
         enum: ['client', 'journalist']
     },
     isVerified: Boolean
 })
+
 module.exports = mongoose.model('User', userSchema)
