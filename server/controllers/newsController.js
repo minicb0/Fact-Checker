b7a26a71a575c1cd591d38e79de101683c863b5a
Santhosh <santhoshm12052003@gmail.com>
2021-09-25 18:58:58 +0530

    basic setup successful

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -11,10 +11,32 @@ exports.createNews = async(req, res) => {
         res.status(201).json({
             message: 'News posted successfully'
         })
-    } catch {
+    } catch (err) {
         res.status(500).json({
             message: 'Server error'
         })
     }
 
+}
+exports.renderNews = async(req, res) => {
+    try {
+        const postId = req.params.id
+        console.log(postId)
+        const postDetails = await Post.findById(postId)
+        console.log(postDetails)
+        if (postDetails) {
+            res.status(200).json({
+                data: postDetails
+            })
+        } else {
+            res.status(404).json({
+                message: "postId not found"
+            })
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
--- server/routes/newsRoute.js
+++ server/routes/newsRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createNews } = require('../controllers/newsController');
+const { createNews, renderNews } = require('../controllers/newsController');
+
 const router = express.Router();
 
 router.post('/news/check', createNews);
-
+router.get('/news/:id', renderNews)
 module.exports = router;
