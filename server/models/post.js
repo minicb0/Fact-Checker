3577ae92e9b3cbd5f85b21d4b610e9e47ebe5d73
Santhosh <74037707+SANTHOSH17-DOT@users.noreply.github.com>
2021-09-25 11:59:54 +0530

    model setup completed (#1)
    * model setup completed
    
    * added .env.example
    
    * comment field change made

---
+++ server/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    assignedJournalists: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    content: String,
+    title: String,
+    status: String,
+    votes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vote'
+    }],
+    endTime: {
+        type: Date,
+        required: true
+    }
+
+})
+module.exports = mongoose.model('Post', postSchema)
---
+++ server/models/vote.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const voteSchema = new mongoose.Schema({
+    JournalistId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+    rating: {
+        min: {
+            type: Number,
+            min: 1
+        },
+        max: {
+            type: Number,
+            max: 5
+        },
+
+    },
+    comment: String
+})
+module.exports = mongoose.model('Vote', voteSchema)
