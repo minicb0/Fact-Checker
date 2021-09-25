152fa10f33887d87a20547bdae434b292b407bcc
geforce6t <shashwat.live@outlook.com>
2021-09-25 12:28:27 +0530

    feat(sample-endpoint): add sample endpoint

---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+//const User = require('../models/user');
+
+exports.createUser = async (req, res) => {
+    return res.status(200).json({
+        message: 'success',
+    });
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors())
 app.use(express.json());
+app.use('/api', require('./routes/userRoute'));
 
 (
     async () => {
