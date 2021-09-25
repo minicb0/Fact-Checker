46505f29d9cf9675a4de3a4eeadf853d39f82378
Santhosh <santhoshm12052003@gmail.com>
2021-09-25 17:51:02 +0530

    basic setup completed

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { createUser } = require('../controllers/userController');
+const dashboardRouter = require('../controllers/dashBoard')
 const router = express.Router();
 
 router.post('/createUser', createUser);
-
+router.get('/dashboard', dashboardRouter);
 module.exports = router;
